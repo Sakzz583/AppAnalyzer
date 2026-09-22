ef6612d1f4868e0f6ea8331fbc99b958063f69fb
Sakzz583 <wsakshi203@gmail.com>
2026-09-22 22:20:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -171,11 +171,13 @@ def upload():
         except OSError:
             pass
 
-
 if __name__ == "__main__":
-    app.run(
-        debug=True
-    )
+    app.run(host="0.0.0.0", port=5000)
+
+#if __name__ == "__main__":
+#    app.run(
+ #       debug=True
+  #  )
 
 # """
 # app.py
